b54268a8287c6536ee61a79db685c48305e7e54c
luke-walsh <68659121+luke-walsh@users.noreply.github.com>
2024-08-02 20:21:36 +0100

    set up interfaces for user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +16,37 @@ import {MatIconModule} from '@angular/material/icon';
     MatCardModule,
     MatProgressBarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule
+  ],
+  providers: [  
+    MatDatepickerModule,
+    MatNativeDateModule  
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
+
 export class AppComponent {
   title = 'run-diary';
+
+}
+
+export interface ICoordinates {
+  lat:  number,
+  long: number
+}
+
+export interface IGoal {
+  placeName: string,
+  progress:  number,
+  coords:    ICoordinates
+}
+
+export interface IUserData {
+  username:      string,
+  totalDistance: number,
+  goals:         IGoal[]
 }
